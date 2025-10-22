952250897ecec9006a8088ab2e154f998b289c34
dimas <dimasnugrohopro22@gmail.com>
2025-10-22 14:22:51 +0700

    UTS: Selesai Soal 3 - Implementasi Flutter TTS

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,15 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:ocr_sederhana/screens/home_screen.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
+  // ubah menjadi statefulwidget
   final String ocrText;
 
   const ResultScreen({super.key, required this.ocrText});
 
   @override
+  State<ResultScreen> createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  late String ocrText; // variabel untuk menyimpan teks OCR
+  late FlutterTts flutterTts; // inisialisasi flutter tts
+
+  // inisialisasi initstate()
+  @override
+  void initState() {
+    super.initState();
+    ocrText = widget.ocrText; // mengambil teks OCR dari widget
+
+    // inisialisasi flutter tts
+    flutterTts = FlutterTts();
+    // mengatur bahasa ke indonesia
+    flutterTts.setLanguage("id-ID");
+  }
+
+  // implementasi dispose
+  @override
+  void dispose() {
+    flutterTts.stop(); // menghentikan text to speech saat dispose
+    super.dispose();
+  }
+
+  // function speak
+  Future<void> _speak(String text) async {
+    if (text.isNotEmpty) {
+      await flutterTts.speak(text);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Hasil OCR')),
+      // appBar: AppBar(title: const Text('Hasil OCR')), // menghilangkan appbar
+      appBar: AppBar(
+        // memodifikasi appbar dengan menambahkan action button
+        title: const Text('Hasil OCR'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.volume_up),
+            tooltip: 'Bacakan Teks',
+            onPressed: () {
+              _speak(ocrText); // memanggil fungsi speakText
+            },
+          ),
+        ],
+      ),
 
       body: Padding(
         padding: const EdgeInsets.all(16.0),
